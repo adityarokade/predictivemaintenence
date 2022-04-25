691441fe246de204263f6e7df62f8ada570de28c
adityarokade <rokadeaditya2002@gmail.com>
2022-04-25 18:01:14 +0530

    re  commit

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 
-
+from flask import Flask
 
 from wsgiref import simple_server
 
+from flask import Flask, session, request, Response, jsonify
 
-
-from flask import Flask,flash,redirect,render_template,request,session,Response, jsonify
-from Training import Training_Validation
-from Prediction import Prediction
-from File_operations.File_operation import File_operation
-import os
-import shutil
-import pandas as pd
-import os
 
 
 import atexit
@@ -21,53 +13,9 @@ import os
 app = Flask(__name__)
 
 
-@app.route('/train',methods=['GET','POST'])
-def training():
-    path="archive/CMaps/"
-    train=Training_Validation(path)
-    score,score1=train.Data_Validation()
-    
-    print("Train Complete",score,score1)
-    return render_template('index.html')
-
-
-@app.route('/',methods=['GET','POST'])
-def Predict():
-    if request.method=='POST':
-        file=request.form['file']
-
-        df=pd.read_csv(file,sep=" ",header=None)
-        Input_File_Path="Prediction_Files/Input.csv"
-        File_Operation=File_operation()
-        File_Operation.Delete_Existing_File(Input_File_Path)
-        df.to_csv(Input_File_Path,sep=",")
-       
-        pred=Prediction().Prediction()
-       
-    
-
-
-
-              
-
-
-
-
-    return render_template('index.html')
-
-@app.route('/Train_Profile_Report',methods=['GET','POST'])
-def Train_profile_Report():
-    return render_template("Train_Data_Profiling.html")
-
-
-
-@app.route('/Test_Profile_Report',methods=['GET','POST'])
-def Test_profile_Report():
-    return render_template("Test_Data_Profiling.html")
-
-
-
-
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return "Flask app is running to good to go arif kkkkkkkkkkkkkkk is chutiya"
 
 port = int(os.getenv("PORT", 5001))
 
