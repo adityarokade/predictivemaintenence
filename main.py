6c15673f34e88c23acd261fdeedc0ee9366bfa60
adityarokade <rokadeaditya2002@gmail.com>
2022-04-26 15:28:11 +0530

    3  commit

--- main.py
+++ main.py
@@ -18,6 +18,18 @@ def index():
     return render_template('index.html')
     # return "Flask app is running to good to go pankaj is chutiya"
 
+@app.route('/Train_Profile_Report',methods=['GET','POST'])
+def Train_profile_Report():
+    return render_template("Train_Data_Profiling.html")
+
+
+
+@app.route('/Test_Profile_Report',methods=['GET','POST'])
+def Test_profile_Report():
+    return render_template("Test_Data_Profiling.html")
+
+
+
 port = int(os.getenv("PORT", 5001))
 
 if __name__ == "__main__":
