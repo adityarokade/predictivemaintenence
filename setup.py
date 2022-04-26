56ac3b7d9201ec2cd738bb85eba65f0f61f9fa21
adityarokade <rokadeaditya2002@gmail.com>
2022-04-26 16:03:11 +0530

    train3  commit

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="docker-heroku",
     version="0.0.3",
-    description="ML project",
-    author="Avnish yadav", 
+    description="ML projects",
+    author="Aditya Rokade", 
     packages=find_packages(),
     license="MIT"
 )
